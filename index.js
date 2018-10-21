baf68296e3c05c7f83d0f6ac1269a355d1a5c56d
Rob Blanckaert <basicer@basicer.com>
2018-10-21 00:40:40 -0700

    Morgan

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
 var path = require('path');
 var public = path.join(__dirname, 'public');
+
+app.set('trust proxy', true);
+app.use(morgan());
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
